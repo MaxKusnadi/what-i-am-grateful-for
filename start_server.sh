f49ec26abcf26e51ba6205e3efbaf5c45f96b0cd
maxkusnadi <max.kusnadi@gmail.com>
2017-04-19 16:43:58 +0000

    finish css for both gratitudes and prayer

--- start_server.sh
+++ start_server.sh
@@ -2,4 +2,4 @@
 
 git pull
 kill $(ps aux | grep 'gunicorn' | awk '{print $2}')
-nohup gunicorn -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -w 3 -b 0.0.0.0:8000 wsgi:app &
+nohup gunicorn -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -w 1  -b 0.0.0.0:8000 wsgi:app &
