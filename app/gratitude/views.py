e4ecaaecab2fe5a3947c2c1a3d61f55cb6f29bee
Max Kusnadi <max.kusnadi@gmail.com>
2017-04-22 00:17:52 +0800

    finsihed navbar, textarea and button color schemes

--- app/gratitude/views.py
+++ app/gratitude/views.py
@@ -16,4 +16,18 @@ class GratitudeView(MethodView):
         return render_template('gratitude/index.html', results=results,
                                title=title)
 
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
+
 app.add_url_rule('/', view_func=GratitudeView.as_view('gratitude'))
--- app/prayer/views.py
+++ app/prayer/views.py
@@ -18,4 +18,18 @@ class PrayerView(MethodView):
                                results=unprayed_prayers,
                                prayed_prayers=prayed_prayers)
 
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
+
 app.add_url_rule('/prayer', view_func=PrayerView.as_view('prayer'))
